d5e442ee87bd827ccf58884cb3bf7d64f0bc69af
Marco Elizalde <contacto@marcoelizalde.com>
2021-10-14 11:39:51 -0500

    add(post) basic post method

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from datetime import datetime
 
-# Create your views here.
+posts = [
+    {
+        'name': 'Mont Blac',
+        'user': 'Marco Elizalde',
+        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs '),
+        'picture':  'https://picsum.photos/200/200/?image=1036'
+    },
+    {
+        'name': 'Mont Blac',
+        'user': 'Marco Elizalde',
+        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs '),
+        'picture':  'https://picsum.photos/200/200/?image=1036'
+    },
+    {
+        'name': 'Mont Blac',
+        'user': 'Marco Elizalde',
+        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs '),
+        'picture':  'https://picsum.photos/200/200/?image=1036'
+    }
+]
+
+def list_posts(request):
+    content=[]
+    for post in posts:
+        content.append("""
+        <p><strong>{name}</strong></p>
+        <p><small>{user} - <i>{timestamp}</i></small></p>
+        <figure><img src="{picture}"/></figure>
+        """.format(**post))
+    return HttpResponse('<br>'.join(content))
