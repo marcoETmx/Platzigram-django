f6db537496fd3c3b70997d536b6f2393162b0899
Marco Elizalde <contacto@marcoelizalde.com>
2021-10-20 18:03:05 -0500

    add(add) profile when create profile

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
 from users.models import Profile
+from django.contrib.auth.models import User
 
 
 @admin.register(Profile)
@@ -8,3 +11,34 @@ class ProfileAdmin(admin.ModelAdmin):
     list_editable = ('phone_number', 'website')
     search_fields = ('user__email', 'user__first_name', 'phone_number')
     list_filter = ('created', 'modified', 'user__is_active')
+
+    fieldsets = (
+        ('Profile', {
+            'fields': (('user', 'picture'),),
+        }),
+        ('Extra info', {
+            'fields': (
+                ('website', 'phone_number'),
+                ('biography')
+            ),
+        }),
+        ('Metadata', {
+            'fields': (('created', 'modified',),),
+        })
+    )
+
+    readonly_fields = ('created', 'modified')
+
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+    verbose_name_plural = 'profiles'
+
+
+class UserAdmin(BaseUserAdmin):
+    inlines = (ProfileInline,)
+
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
