d4bd3bd91e211acbce1623020b8b8e348013fc28
Marco Elizalde <contacto@marcoelizalde.com>
2021-10-20 17:44:07 -0500

    add(add) filter custom adim profile

--- users/admin.py
+++ users/admin.py
@@ -6,3 +6,5 @@ from users.models import Profile
 class ProfileAdmin(admin.ModelAdmin):
     list_display = ('pk', 'user', 'phone_number', 'website', 'picture')
     list_editable = ('phone_number', 'website')
+    search_fields = ('user__email', 'user__first_name', 'phone_number')
+    list_filter = ('created', 'modified', 'user__is_active')
