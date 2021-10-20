60c7a7b75cbb9c450d61efba4ec3efa0932a155b
Marco Elizalde <contacto@marcoelizalde.com>
2021-10-20 17:02:27 -0500

    fix(models) delete user model

--- platzigram/urls.py
+++ platzigram/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path
+
 from posts import views as posts_views
 
 urlpatterns = [
+
+    path('admin/', admin.site.urls),
     path('posts/', posts_views.list_posts),
 ]
