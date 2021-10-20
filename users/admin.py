f278d66481aeb519f933467222b82a1b77500ca8
Marco Elizalde <contacto@marcoelizalde.com>
2021-10-20 17:39:38 -0500

    add(add) register and custom adim profile

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from users.models import Profile
 
-# Register your models here.
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'user', 'phone_number', 'website', 'picture')
+    list_editable = ('phone_number', 'website')
