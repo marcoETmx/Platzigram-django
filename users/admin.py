6a30e0cf4c5211dea707b5c893ea2107f6ed0819
Marco Elizalde <contacto@marcoelizalde.com>
2021-10-21 05:55:41 -0500

    add(admin) add new model to admin

--- users/admin.py
+++ users/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
 from users.models import Profile
+from posts.models import Post
 from django.contrib.auth.models import User
 
 
@@ -28,6 +29,13 @@ class ProfileAdmin(admin.ModelAdmin):
     )
 
     readonly_fields = ('created', 'modified')
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'photo')
+    search_fields = ('user__email', 'user__first_name',)
+    list_filter = ('created', 'modified',)
 
 
 class ProfileInline(admin.StackedInline):
