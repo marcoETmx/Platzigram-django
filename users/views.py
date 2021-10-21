7eeb76b6dd2451a23b78a7b83f3d7df97d1f10f3
Marco Elizalde <contacto@marcoelizalde.com>
2021-10-21 13:03:34 -0500

    add(users) add new middleware

--- users/views.py
+++ users/views.py
@@ -49,3 +49,7 @@ def signup(request):
         return redirect('login')
 
     return render(request, 'users/signup.html')
+
+
+def update_profile(request):
+    return render(request, 'users/update_profile.html')
