bce976b29f79f624b3621b1f49ffaa21999deeaa
Marco Elizalde <contacto@marcoelizalde.com>
2021-12-08 14:31:52 -0600

    change

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import login_required
 from datetime import datetime
+from posts.models import Post
+
+from posts.forms import PostForm
 
 posts = [
     {
@@ -35,4 +38,27 @@ posts = [
 
 @login_required
 def list_posts(request):
+    posts = Post.object.all().order_by('-created')
     return render(request, 'posts/feed.html', {'posts': posts})
+
+
+@login_required
+def create_post(request):
+    """Create new post view."""
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('feed')
+    else:
+        form = PostForm()
+
+    return render(
+        request=request,
+        template_name='posts/new.html',
+        context={
+            'form': form,
+            'user': request.user,
+            'profile': request.user.profile
+        }
+    )
--- users/views.py
+++ users/views.py
@@ -52,6 +52,7 @@ def signup(request):
     return render(request, 'users/signup.html')
 
 
+@login_required
 def update_profile(request):
     profile = request.user.profile
 
