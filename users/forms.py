3dd7448d249d4bc59434240d45d842898e3c1e6c
Marco Elizalde <contacto@marcoelizalde.com>
2021-12-08 15:20:45 -0600

    validate forms

--- users/forms.py
+++ users/forms.py
@@ -1,4 +1,47 @@
 from django import forms
+from django.contrib.auth.models import User
+
+from users.models import Profile
+from users.models import User
+
+
+class SignupForm(forms.Form):
+    username = forms.CharField(min_length=4, max_length=50)
+    password = forms.CharField(max_length=70, widget=forms.PasswordInput())
+    password_confirmation = forms.CharField(
+        max_length=70, widget=forms.PasswordInput())
+    first_name = forms.CharField(min_length=2, max_length=50)
+    last_name = forms.CharField(min_length=2, max_length=50)
+
+    email = forms.CharField(
+        widget=forms.EmailField)
+
+    def clean_username(self):
+        """Username must be unique."""
+        username = self.changed_data['username']
+        username_taken = User.objects.filter(username=username).exists()
+        if username_taken:
+            raise forms.ValidationError('Username is already in user')
+        return username
+
+    def clean(self):
+        """Verify password confirmation match"""
+        data = super().clean()
+
+        password = data['password']
+        password_confirmation = data['password_confirmation']
+
+        if password != password_confirmation:
+            raise forms.ValidationError('Password not match')
+        return data
+
+    def save(self):
+        data = self.cleaned_data
+        data.pop('password_confirmation')
+
+        user = User.objects.create(**data)
+        profile = Profile(user=user)
+        profile.save()
 
 
 class ProfileForm(forms.Form):
--- users/views.py
+++ users/views.py
@@ -2,6 +2,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect, render
 from users.forms import ProfileForm
+from users.forms import ProfileForm, SignupForm
 
 
 def login_view(request):
@@ -26,30 +27,20 @@ def logout_view(request):
 
 
 def signup(request):
+    """Sign up view."""
     if request.method == 'POST':
-        username = request.POST['username']
-        passwd = request.POST['passwd']
-        passwd_confirmation = request.POST['passwd_confirmation']
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = SignupForm()
 
-        if passwd != passwd_confirmation:
-            return render(request, 'users/signup.html', {'error': 'Password confirmation does not match'})
-
-        try:
-            user = User.objects.create_user(username=username, password=passwd)
-        except IntegrityError:
-            return render(request, 'users/signup.html', {'error': 'Username is already in user'})
-
-        user.first_name = request.POST['first_name']
-        user.last_name = request.POST['last_name']
-        user.email = request.POST['email']
-        user.save()
-
-        profile = Profile(user=user)
-        profile.save()
-
-        return redirect('login')
-
-    return render(request, 'users/signup.html')
+    return render(
+        request=request,
+        template_name='users/signup.html',
+        context={'form': form}
+    )
 
 
 @login_required
